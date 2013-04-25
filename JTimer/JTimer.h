0c4b290ecf1d40bca28322429cb7f40081e3d9b2
Jeremy Tregunna <jeremy@tregunna.ca>
2013-04-25 08:50:03 -0600

    Initial Commit

---
+++ JTimer/JTimer.h
@@ -0,0 +1,13 @@
+//
+//  JTimer.h
+//  JTimer
+//
+//  Created by Jeremy Tregunna on 2013-04-25.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTimer : NSObject
+
+@end
---
+++ JTimerTests/JTimerTests.h
@@ -0,0 +1,13 @@
+//
+//  JTimerTests.h
+//  JTimerTests
+//
+//  Created by Jeremy Tregunna on 2013-04-25.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JTimerTests : SenTestCase
+
+@end
